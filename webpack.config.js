017bfd5d816bfee43b78f40b09011527e0f5e73f
Shin'ya Ueoka <ueokande@i-beam.org>
2017-09-09 11:26:58 +0900

    install webpack and babel

---
+++ webpack.config.js
@@ -0,0 +1,33 @@
+const path = require('path');
+
+const src = path.resolve(__dirname, 'src');
+const dist = path.resolve(__dirname, 'build');
+
+module.exports = {
+  entry: {
+    content: path.join(src, 'content'),
+    background: path.join(src, 'background')
+  },
+
+  output: {
+    path: dist,
+    filename: '[name].js'
+  },
+
+  module: {
+    loaders: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+        query: {
+          presets: [ 'es2015' ]
+        }
+      },
+    ]
+  },
+
+  resolve: {
+    extensions: [ '.js' ]
+  }
+};
